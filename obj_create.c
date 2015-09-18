a367f52fa52a5b792f961c2f651d205ec25b1e27
Victor Wagner <wagner@atlas-kard.ru>
2015-09-18 11:21:50 +0300

    Added 'fake' OIDs for oid-less objects. Unfortunately, it seems to be impossible to add oid-less object to object datahbase at the runtime. Removed extra copy operation from gost_add_obj

--- obj_create.c
+++ obj_create.c
@@ -5,8 +5,6 @@
 int gost_add_obj(const char *oid, const char *sn, const char *ln)
 {
    	int nid;
-	char *oidtemp=NULL,*sntemp=NULL,*lntemp=NULL;
-	
    	if (oid) {
    		nid = OBJ_txt2nid(oid);
 	} else {
@@ -14,20 +12,6 @@ int gost_add_obj(const char *oid, const char *sn, const char *ln)
 	}
    	if (nid != NID_undef) {
 		return nid;
-	}
-	if (oid) {
-		oidtemp=OPENSSL_malloc(strlen(oid) + 2);
-		strcpy(oidtemp, oid);
-	}
-
-	if (sn) {
-		sntemp=OPENSSL_malloc(strlen(sn) + 2);
-		strcpy(sntemp, sn);
-	}
-
-	if (ln) {
-		lntemp=OPENSSL_malloc(strlen(ln) + 2);
-		strcpy(lntemp, ln);
 	}
 	return OBJ_create(oid,sn,ln);
 }
