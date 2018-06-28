0f4c57408321d40b7c812969997fc923e138414b
Ilya Shipitsin <chipitsine@gmail.com>
2018-06-28 13:29:53 +0500

    move .ci scripts into .travis.yml

--- .ci/build-openssl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-
-git clone -b ${OPENSSL_BRANCH} https://github.com/openssl/openssl.git
-
-cd openssl
-./config shared --prefix=${PREFIX} --openssldir=${PREFIX}
-make all install_sw
