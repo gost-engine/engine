0b50e0028fa9d2cfb9648f8f3209e89d75e311c1
Dmitry Belyavskiy <beldmit@gmail.com>
2018-11-18 15:06:43 +0300

    GOST md aliases

--- gost_eng.c
+++ gost_eng.c
@@ -122,6 +122,8 @@ static int gost_engine_finish(ENGINE* e) {
 }
 
 static int gost_engine_destroy(ENGINE* e) {
+    EVP_delete_digest_alias("streebog256");
+    EVP_delete_digest_alias("streebog512");
     digest_gost_destroy();
     digest_gost2012_256_destroy();
     digest_gost2012_512_destroy();
@@ -290,6 +292,11 @@ static int bind_gost(ENGINE* e, const char* id) {
 /*        || !EVP_add_digest(magma_omac_acpkm()) */
         || !EVP_add_digest(grasshopper_omac_acpkm())
             ) {
+        goto end;
+    }
+
+    if(!EVP_add_digest_alias(SN_id_GostR3411_2012_256, "streebog256")
+       ||	!EVP_add_digest_alias(SN_id_GostR3411_2012_512, "streebog512")) {
         goto end;
     }
 
