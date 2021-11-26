gost-engine/engine

commit 0755b6e5faa6f32ed64357a78f1b5408e4d710e8

gosthash2012: Properly ifdef '_mm_empty' call
There is other architectures besides __x86_64__, so EMMS should be
issued on IA-32 only where we are using MMX.

gosthash2012.c
@@ -135,7 +135,7 @@ static void g(union uint512_u *h, const union uint512_u * RESTRICT N,
     X128R(xmm0, xmm2, xmm4, xmm6, xmm1, xmm3, xmm5, xmm7);
 
     STORE(h, xmm0, xmm2, xmm4, xmm6);
-# ifndef __x86_64__
+# ifndef __i386__
     /* Restore the Floating-point status on the CPU */
     /* This is only required on MMX, but EXTRACT32 is using MMX */
     _mm_empty();

commit bb614dc9c3eefb64979485844f8527054d097a58

gosthash2012: Fix '_mm_empty' call
'_mm_empty' call incorrectly wrapped in `#ifndef' instead of `#ifdef'.
This was affecting correctness of benchmarks on i686:

  type         16 bytes  64 bytes  256 bytes  1024 bytes  8192 bytes  16384 bytes
  streebog512      -nan      -nan       -nan        -nan        -nan         -nan

Fixes: 0755b6e ("gosthash2012: Properly ifdef '_mm_empty' call")
Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

gosthash2012.c
@@ -135,7 +135,7 @@ static void g(union uint512_u *h, const union uint512_u * RESTRICT N,
     X128R(xmm0, xmm2, xmm4, xmm6, xmm1, xmm3, xmm5, xmm7);
 
     STORE(h, xmm0, xmm2, xmm4, xmm6);
-# ifndef __i386__
+# ifdef __i386__
     /* Restore the Floating-point status on the CPU */
     /* This is only required on MMX, but EXTRACT32 is using MMX */
     _mm_empty();