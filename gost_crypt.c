63f68e816c06162d2e23a06d4059136104fe77d2
igrkir <i.kirillov@kryptonite.ru>
2022-09-23 15:32:21 +0300

    fix function error name mismatches

--- gost_crypt.c
+++ gost_crypt.c
@@ -1001,7 +1001,7 @@ static int magma_cipher_ctl(EVP_CIPHER_CTX *ctx, int type, int arg, void *ptr)
     case EVP_CTRL_RAND_KEY:
             if (RAND_priv_bytes
                 ((unsigned char *)ptr, EVP_CIPHER_CTX_key_length(ctx)) <= 0) {
-                GOSTerr(GOST_F_GOST_CIPHER_CTL, GOST_R_RNG_ERROR);
+                GOSTerr(GOST_F_MAGMA_CIPHER_CTL, GOST_R_RNG_ERROR);
                 return -1;
             }
             break;
